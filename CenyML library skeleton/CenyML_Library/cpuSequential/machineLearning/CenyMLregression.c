a234a91644e983609c8061ed847cc66ea271ab7c
Cesar Miranda Meza <cmirandameza3@hotmail.com>
2021-11-15 05:00:48 +0000

    I changed the values assigned to the degrees of freedom for the mean square error in the CenyML library for the main.c file in which I did the simple linear regression validation. Then, for the python file of that same validation algorithm, I gave it the ability to compare the CenyML coefficients obtained with those identified in the reference library. In addition, I improved the conditionals that validate the argument variables in the get and predict functions of the simple linear regression algorithm contained in the CenyML library. Next, I began writing the get function for multiple linear regression in the CenyMl library but I did not finished it.

--- CenyML library skeleton/CenyML_Library/cpuSequential/machineLearning/CenyMLregression.c
+++ CenyML library skeleton/CenyML_Library/cpuSequential/machineLearning/CenyMLregression.c
@@ -80,12 +80,12 @@
 */
 void getSimpleLinearRegression(double *X, double *Y, int n, int m, int p, double *b) {
 	// If the machine learning features exceed the value of one, then emit an error message and terminate the program. Otherwise, continue with the program.
-	if (m > 1) {
+	if (m != 1) {
 		printf("\nERROR: The machine learning features (independent variables) must be equal to 1 for this particular algorithm.\n");
 		exit(1);
 	}
 	// If the output of the system under study exceed the value of one, then emit an error message and terminate the program. Otherwise, continue with the program.
-	if (p > 1) {
+	if (p != 1) {
 		printf("\nERROR: The outputs of the system under study must be equal to 1 for this particular algorithm.\n");
 		exit(1);
 	}
@@ -167,12 +167,12 @@ void getSimpleLinearRegression(double *X, double *Y, int n, int m, int p, double
 */
 void predictSimpleLinearRegression(double *X, double *b, int n, int m, int p, double *Y_hat) {
 	// If the machine learning features exceed the value of one, then emit an error message and terminate the program. Otherwise, continue with the program.
-	if (m > 1) {
+	if (m != 1) {
 		printf("\nERROR: The machine learning features (independent variables) must be equal to 1 for this particular algorithm.\n");
 		exit(1);
 	}
 	// If the output of the system under study exceed the value of one, then emit an error message and terminate the program. Otherwise, continue with the program.
-	if (p > 1) {
+	if (p != 1) {
 		printf("\nERROR: The outputs of the system under study must be equal to 1 for this particular algorithm.\n");
 		exit(1);
 	}
@@ -181,5 +181,125 @@ void predictSimpleLinearRegression(double *X, double *b, int n, int m, int p, do
 	for (int currentRow = 0; currentRow < n; currentRow++) {
 		Y_hat[currentRow] = b[0] + b[1]*X[currentRow];
 	}
+}
+
+
+/**
+* The "getSimpleLinearRegression()" function is used to apply the
+* machine learning algorithm called simple linear regression. Within
+* this process, the best fitting equation with the form of "y_hat =
+* b_0 + b_1*x" will be identified with respect to the sampled data
+* given through the argument pointer variables "X" and "Y". As a
+* result, the identified coefficient values will be stored in the
+* argument pointer variable "b".
+* 
+* @param double *X - This argument will contain the pointer to a
+*					 memory allocated input matrix, from which the
+*					 desired machine learning algorithm will be
+*					 calculated. THIS VARIABLE SHOULD BE ALLOCATED
+*					 AND INNITIALIZED BEFORE CALLING THIS FUNCTION
+*					 WITH A SIZE OF "n" TIMES "m=1" 'DOUBLE' MEMORY
+*					 SPACES.
+*
+* @param double *Y - This argument will contain the pointer to a
+*					 memory allocated output matrix, representing
+*					 the real data of the system under study. This
+*					 variable will be used as a reference to apply
+*					 the desired machine learning algorithm. THIS
+*					 VARIABLE SHOULD BE ALLOCATED AND INNITIALIZED
+*					 BEFORE CALLING THIS FUNCTION WITH A SIZE OF
+*					 "n" TIMES "p=1" 'DOUBLE' MEMORY SPACES.
+*
+* @param int n - This argument will represent the total number of
+*				 samples (rows) that the input matrix has, with which 
+*				 the output data was obtained.
+*
+* @param int m - This argument will represent the total number of
+*				 features (independent variables) that the input
+*				 matrix has, with which the output data was obtained.
+*
+* @param int p - This argument will represent the total number of 
+*				 outputs that exist in the the output matrix,
+*				 containing the real results of the system under
+*				 study.
+*
+* @param double *b - This argument will contain the pointer to a
+*					 memory allocated variable in which we will store
+*					 the identified best fitting coefficient values
+*					 for the desired machine learning algorithm. These
+*					 coefficients will each be stored in the same
+*					 column but under different rows where the first
+*					 coefficient (b_0) will be stored in the row with
+*					 index 0 and the last coefficient (b_1) will be
+*					 stored in the row with index 1. IT IS
+*					 INDISPENSABLE THAT THIS VARIABLE IS ALLOCATED
+*					 BEFORE CALLING THIS FUNCTION WITH A VARIABLE SIZE
+*					 OF "m+1=2" 'DOUBLE' MEMORY SPACES.
+*
+* NOTE: RESULT IS STORED IN THE MEMORY ALLOCATED POINTER VARIABLE "b".
+* 
+* @return void
+*
+* @author Miranda Meza Cesar
+* CREATION DATE: NOVEMBER 13, 2021
+* LAST UPDATE: N/A
+*/
+void getMultipleLinearRegression(double *X_tilde, double *Y, int n, int m, int p, char isVariableOptimizer, double *b) {
+	// If the machine learning features exceed the value of one, then emit an error message and terminate the program. Otherwise, continue with the program.
+	if (m < 1) {
+		printf("\nERROR: The machine learning features (independent variables) must be equal or greater than 1 for this particular algorithm.\n");
+		exit(1);
+	}
+	// If the output of the system under study exceed the value of one, then emit an error message and terminate the program. Otherwise, continue with the program.
+	if (p != 1) {
+		printf("\nERROR: The outputs of the system under study must be equal to 1 for this particular algorithm.\n");
+		exit(1);
+	}
+	
+	// We declare and innitialize the variable "new_X_tilde", which will be used to store certain arrangements of the input data and will be used instead of "X_tilde".
+	int mPlusOne = m+1; //This variable is used to store a repetitive matheamtical operation, for performance purposes.
+	new_X_tilde = (double *) malloc(n*mPlusOne*sizeof(double)); // We allocate the memory required for the local pointer variable "new_X_tilde".
+	int currentRowTimesM; // This variable is used to store a repetitive multiplication in some for-loops, for performance purposes.
+	int currentRowAndColumn; // This variable is used to store a repetitive mathematical operations in some for-loops, for performance purposes.
+	for (int currentRow=0; currentRow<n; currentRow++) { // We initialize the first arrangement to be used from the input matrix, which is the data as it originally is received on this function.
+		currentRowTimesM = currentRow*m;
+		for (int currentColumn=0; currentColumn<mPlusOne; currentColumn++) {
+			currentRowAndColumn = currentColumn + currentRowTimesM;
+			new_X_tilde[currentRowAndColumn] = X_tilde[currentRowAndColumn];
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// We declare and innitialize the variables that will be required to calculate the coefficients of "b".
+	double sumOf_xy = 0;
+	double sumOf_y = 0;
+	double sumOf_x = 0;
+	double sumOf_xSquared = 0;
+	
+	// We calculate the necessary summations in order to obtain the coefficients of "b".
+	for (int currentRow = 0; currentRow < n; currentRow++) {
+		sumOf_xy += X[currentRow] * Y[currentRow];
+		sumOf_y += Y[currentRow];
+		sumOf_x += X[currentRow];
+		sumOf_xSquared += X[currentRow] * X[currentRow];
+	}
+	
+	// We calculate the value of the coefficient "b_1".
+	b[1] = (n*sumOf_xy - sumOf_y*sumOf_x)/(n*sumOf_xSquared - sumOf_x*sumOf_x);
+	
+	// We calculate the value of the coefficient "b_0".
+	b[0] = (sumOf_y - b[1]*sumOf_x)/n;
 }
 
