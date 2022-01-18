4b4832765e84f0ae1edbb4e80fb36aff86011ba1
Cesar Miranda Meza <cmirandameza3@hotmail.com>
2022-01-17 23:49:55 -0800

    CenyML Library v0.113.73.29

--- CenyML library skeleton/CenyML_Library/cpuSequential/machineLearning/CenyMLclassification.c
+++ CenyML library skeleton/CenyML_Library/cpuSequential/machineLearning/CenyMLclassification.c
@@ -1,5 +1,5 @@
 /*
-   Copyright 2021 Cesar Miranda Meza (alias: Mortrack)
+   Copyright 2022 Cesar Miranda Meza (alias: Mortrack)
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
